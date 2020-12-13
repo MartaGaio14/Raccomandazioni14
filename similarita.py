8a105dee6382337eb61565e2e31ec51a94748430
MartaGaio14 <gaio.marta@gmail.com>
2020-12-13 11:55:27 +0100

    la similarità

--- similarita.py
+++ similarita.py
@@ -1,1 +1,19 @@
+#funzioni per calcolare la similarità
 
+def compara(dictU, dictA):
+    utente=[]
+    articolo=[]
+    for keyU in dictU:
+        for keyA in dictA:
+            if keyU == keyA:
+                utente.append(dictU[keyA])
+                articolo.append(dictA[keyA])
+    return utente,articolo
+            
+def CosDist(u, v):
+    dist = np.dot(u, v) / (LA.norm(u) * LA.norm(v))
+    return dist
+
+def similarità(dictU, dictA):
+    (a,b)=compara(dictU, dictA)
+    return CosDist(a,b)
