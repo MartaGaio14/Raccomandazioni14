471f4cab76785931cdb3c68e3e7cb1ca388e11e4
MartaGaio14 <gaio.marta@gmail.com>
2020-12-14 15:30:34 +0100

    multithreading

--- profili_utenti.py
+++ profili_utenti.py
@@ -1,4 +1,9 @@
 import tqdm
+import multiprocessing as mp
+import os
+import threading
+
+
 def ContentBasedProfile(Hist_0, dimensioni, pesi):
     #lista di liste di due elementi ciascuna: numero del topic+somma dei pesi corrispondenti
     somme=[] 
@@ -23,29 +28,54 @@ def ContentBasedProfile(Hist_0, dimensioni, pesi):
 #doc_tfidf lista di dizionari con le rappresentazioni in tfidf di tutte le news
 #Hist lista di liste delle news lette da ciascun utente
 #S_norep lista intera delle news di training lette dalla totalità degli utenti
-def utenti_tfidf(Hist,doc_tfidf, S_norep):
-    u_profile_tfidf=[]#lista di dizionari, uno per ogni utente
-    for i in tqdm.tqdm(range(len(Hist))): #i gira negli user
-        testi=[]#lista di dizionari, ogni dizionario è una news in tfidf letta dall'utente
+
+#parlallelizziamo
+    
+
+
+#calcola il profilo di un utente 
+def utenti_tfidf(Hist_u, doc_tfidf, S_norep, coda= None):
+   testi=[]#lista di dizionari, ogni dizionario è una news in tfidf letta dall'utente
         #in questione (l'iesimo)
-        for j in range(len(doc_tfidf)):
-            if S_norep[j] in Hist[i]:
-                testi.append(doc_tfidf[j])
-        diz={}
+   for j in range(len(doc_tfidf)):
+       if S_norep[j] in Hist_u:
+           testi.append(doc_tfidf[j])
+   diz={}
         #dimensioni-->lista di dimensioni (riferita a tutte le news salvate in testi)
         #pesi-->lista dei pesi corrispondenti (i pesi sono le chiavi del dizionario)
-        for t in range(len(testi)):
-            pesi=list(testi[t].values())
-            dimensioni=list(testi[t].keys())
-            for j in range(len(testi[t])):
-                diz[dimensioni[j]]=pesi[j]
-        profilo=dict(ContentBasedProfile(Hist[i],list(diz.keys()),list(diz.values())))
-        if len(profilo)>1000:
-            profilo=dict(sorted(profilo.items(), key=lambda item: item[1])[0:1000])
-        u_profile_tfidf.append(profilo)
+   for t in range(len(testi)):
+        pesi=list(testi[t].values())
+        dimensioni=list(testi[t].keys())
+        for j in range(len(testi[t])):
+            diz[dimensioni[j]]=pesi[j]
+   profilo=dict(ContentBasedProfile(Hist_u,list(diz.keys()),list(diz.values())))
+   if len(profilo)>1000:
+        profilo=dict(sorted(profilo.items(), key=lambda item: item[1])[0:1000])
+   if coda is None:#se è non valorizzato ritorno il profilo
+        return profilo
+   else:#altrimenti accodo il profilo che ho trovato
+        coda.put(profilo)
+        
+        
+
+def utenti_tfidf_par(Hist, doc_tfidf, S_norep):
+    coda = mp.Queue()
+    threads = [threading.Thread(target=utenti_tfidf, args=(Hist_u,), 
+        kwargs={"doc_tfidf": doc_tfidf,"S_norep": S_norep, "coda": coda}) 
+               for Hist_u in tqdm.tqdm(Hist)]
+    for t in threads2:
+        t.start()
+    u_profile_tfidf=[coda.get() for t in threads]
+    for t in threads:
+        t.join() # blocca il MainThread finché t non è completato
     return u_profile_tfidf
 
-
+coda2 = mp.Queue()
+threads2 = [threading.Thread(target=utenti_tfidf, args=(Hist_u,), 
+        kwargs={"doc_tfidf": doc_tfidf,"S_norep": S_norep, "coda": coda2}) 
+               for Hist_u in tqdm.tqdm(Hist)]
+threads2[999].start()
+coda2.get()
 ###################CONTENT BASED PROFILE IN RAPPRESENTAZIONE LDA
 #doc_lda lista di dizionari con le rappresentazioni in lda di tutte le news
 #Hist lista di liste delle news lette da ciascun utente
