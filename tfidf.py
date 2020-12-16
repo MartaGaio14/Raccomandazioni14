9a7c2e441a5af54aa9e10ad1185978c5401a89e5
Irene Chioatto <irene@Air-di-Irene.homenet.telecomitalia.it>
2020-12-16 22:02:42 +0100

    tfidf parallelo

--- tfidf.py
+++ tfidf.py
@@ -1,6 +1,7 @@
 #TFIDF
 import numpy as np
 import tqdm
+import multiprocessing as mp
 
 def CountFreq(word_list):
     word_dict={} 
@@ -33,6 +34,43 @@ def TF_IDF(texts):
         tfidf_doc=dict(tfidf_doc)
         if len(tfidf_doc)>1000:
             tfidf_corpus.append(dict(sorted(tfidf_doc.items(), key=lambda item: item[1], reverse=True)[0:1000]))
+            #ordina gli elementi del dizionario e la chiave di ordinamento è il peso tfidf (cioè item[1] nella coppia chiave-valore)
         else:
             tfidf_corpus.append(tfidf_doc)
-    return tfidf_corpus   
+    return tfidf_corpus  
+
+#testo=texts[0]
+
+def TF_IDF2(testo, N, n_i, num):
+     #numero di documenti nel corpus
+    tot_doc=CountFreq(testo) # dizionario freq di un documento
+    #for j in tqdm.tqdm(range(0,N)):
+    k=list(tot_doc.keys())
+    tfidf_doc={}
+    for i in range(0, len(tot_doc)):
+        max_f=max(list(tot_doc.values())) #parola con massima freq nel documento j
+        tf=tot_doc[k[i]]/max_f #numero di occorrenze del termine i nel documento j/max_f
+        idf=np.log10(N/n_i[k[i]]) #n_i[k[i]] n documenti che contengono termine i
+        tfidf_doc[k[i]]= tf*idf
+    if len(tfidf_doc)>num:
+        tfidf_doc=dict(sorted(tfidf_doc.items(), key=lambda item: item[1], reverse=True)[0:num])
+        #ordina gli elementi del dizionario e la chiave di ordinamento è il peso tfidf (cioè item[1] nella coppia chiave-valore)
+    return tfidf_doc
+
+#TF_IDF2(texts[0],N,n_i, num)
+
+from functools import partial
+from itertools import repeat
+
+def TFIDF_par(testi, N, n_i, num):
+    N_CPU = mp.cpu_count()
+    pool=mp.Pool(processes=N_CPU)
+    #f=partial(TF_IDF2, N,n_i,num)
+    #t=pool.map(f, testi)
+    t=pool.starmap(TF_IDF2, zip(testi, repeat(N), repeat(n_i), repeat(num)))
+    pool.close()
+    pool.join()
+    return t
+
+
+
