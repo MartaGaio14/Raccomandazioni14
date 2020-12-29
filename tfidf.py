46af2e11e128c44a8842454dab7ea576ddb8490e
MartaGaio14 <gaio.marta@gmail.com>
2020-12-29 19:13:19 +0100

    tf idf separati

--- tfidf.py
+++ tfidf.py
@@ -11,35 +11,72 @@ def CountFreq(word_list):
         else:
             word_dict[word]+=1
     return word_dict  
+#
+# def TF_IDF(texts):
+#     tot_doc=[] #lista di dizionari freq per ogni documento
+#     allwords=[]#lista parole singole per ogni documento
+#     for z in range(0, len(texts)):
+#         b=CountFreq(texts[z])
+#         tot_doc.append(b)
+#         norep=np.unique(texts[z])
+#         allwords.extend(norep)
+#     n_i=CountFreq(allwords) #numero di documenti che contengono un termine
+#     N=len(tot_doc) #numero di documenti nel corpus
+#     tfidf_corpus = []
+#     for j in tqdm.tqdm(range(0,N)):#j è il documento
+#         k=list(tot_doc[j].keys()) #lista delle parole nel documento
+#         tfidf_doc=[]
+#         for i in range(0, len(tot_doc[j])):
+#             max_f=max(list(tot_doc[j].values())) #parola con massima freq nel documento j
+#             tf=tot_doc[j][k[i]]/max_f #numero di occorrenze del termine i nel documento j/max_f
+#             idf=np.log10(N/n_i[k[i]]) #n_i[k[i]] n documenti che contengono termine i
+#             tfidf_doc.append([k[i], tf*idf])
+#         tfidf_doc=dict(tfidf_doc)
+#         if len(tfidf_doc)>750:
+#             tfidf_corpus.append(dict(sorted(tfidf_doc.items(), key=lambda item: item[1], reverse=True)[0:750]))
+#             #ordina gli elementi del dizionario e la chiave di ordinamento è il peso tfidf (cioè item[1] nella coppia chiave-valore)
+#         else:
+#         tfidf_corpus.append(tfidf_doc)
+#     return tfidf_corpus
 
-def TF_IDF(texts):
-    tot_doc=[] #lista di dizionari freq per ogni documento
-    allwords=[]#lista parole singole per ogni documento 
-    for z in range(0, len(texts)): 
-        b=CountFreq(texts[z])
-        tot_doc.append(b) 
-        norep=np.unique(texts[z])
-        allwords.extend(norep)    
-    n_i=CountFreq(allwords) #numero di documenti che contengono un termine 
-    N=len(tot_doc) #numero di documenti nel corpus
+def IDF(texts_train):
+    N=len(texts_train)
+    allwords=[]#lista parole singole del corpus del training set con ripetizioni
+    for z in range(0, len(texts_train)):
+        norep=np.unique(texts_train[z])
+        allwords.extend(norep)
+    n_i=CountFreq(allwords) #numero di documenti che contengono un termine
+    chiavi=list(n_i.keys())
+    for i in range(len(n_i)):
+        n_i[chiavi[i]]=np.log10(N/n_i[chiavi[i]])
+    return n_i
+
+def TFIDF(texts, idf):
+    tot_doc = []  # lista di dizionari freq per ogni documento
+    for z in range(0, len(texts)):
+        b = CountFreq(texts[z])
+        tot_doc.append(b)
     tfidf_corpus = []
-    for j in tqdm.tqdm(range(0,N)):
-        k=list(tot_doc[j].keys())
+    for j in tqdm.tqdm(range(0,len(tot_doc))):#j è il documento
+        k=list(tot_doc[j].keys()) #lista delle parole nel documento j
         tfidf_doc=[]
-        for i in range(0, len(tot_doc[j])):
+        for i in range(len(tot_doc[j])):# i è la parola nel documento j
             max_f=max(list(tot_doc[j].values())) #parola con massima freq nel documento j
             tf=tot_doc[j][k[i]]/max_f #numero di occorrenze del termine i nel documento j/max_f
-            idf=np.log10(N/n_i[k[i]]) #n_i[k[i]] n documenti che contengono termine i
-            tfidf_doc.append([k[i], tf*idf])
+            if k[i] in list(idf.keys()):
+                tfidf_doc.append([k[i], tf*idf[k[i]] ])
+            else:
+                tfidf_doc.append([k[i], 0])
         tfidf_doc=dict(tfidf_doc)
         if len(tfidf_doc)>750:
             tfidf_corpus.append(dict(sorted(tfidf_doc.items(), key=lambda item: item[1], reverse=True)[0:750]))
             #ordina gli elementi del dizionario e la chiave di ordinamento è il peso tfidf (cioè item[1] nella coppia chiave-valore)
         else:
         tfidf_corpus.append(tfidf_doc)
-    return tfidf_corpus  
+    return tfidf_corpus
 
 
+#########questa su un testo solo (per parallelizzare)
 def TF_IDF2(testo, N, n_i, num):
      #numero di documenti nel corpus
     tot_doc=CountFreq(testo) # dizionario freq di un documento
@@ -56,7 +93,6 @@ def TF_IDF2(testo, N, n_i, num):
         #ordina gli elementi del dizionario e la chiave di ordinamento è il peso tfidf (cioè item[1] nella coppia chiave-valore)
     return tfidf_doc
 
-#TF_IDF2(texts[0],N,n_i, num)
 
 from functools import partial
 from itertools import repeat
