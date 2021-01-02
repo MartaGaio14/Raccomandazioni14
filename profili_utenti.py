5ab031d59db14bca1d29c14385a3090fe381b17a
MartaGaio14 <gaio.marta@gmail.com>
2021-01-02 23:35:48 +0100

    profilo utente veloce

--- profili_utenti.py
+++ profili_utenti.py
@@ -2,18 +2,35 @@ from collections import defaultdict
 # storia=n_train[0]
 # lista_tuple=doc_lda_train
 # ID=ID_train
-
+def profilo(storia, lista_tuple):
+   testi=[]#lista delle rappresentazioni concatenate di tutte le news lette dall'utente
+   for articolo in storia: # selezioniamo solo le news in storia
+       testi.extend(lista_tuple[articolo])
+   t = list(zip(*testi))  # separiamo le tuple(etichetta, peso)
+   etichetta=list(t[0])
+   peso=list(t[1])
+   #in un dizionario salviamo (etichetta, somme dei pesi associati all'etichetta che compaiono in testi)
+   profilo = defaultdict(int)
+   for j in range(len(etichetta)):
+        profilo[etichetta[j]] += peso[j]
+    #dividiamo ciascun valore del dizionario per len(storia) per ottenere la media
+   chiavi=list(profilo.keys())
+   for chiave in chiavi:
+       profilo[chiave] /= len(storia)
+    #teniamo solo i primi 1000 elemeti della rappresentazione (utile per tfidf)
+   if len(profilo)>1000:
+        profilo=dict(sorted(profilo.items(), key=lambda item: item[1])[0:1000])
+   return profilo
 # calcola il profilo content based di un utente u
 # storia : History dell'utente u
 # lista_tuple : lista con le rappresentazioni (tfidf o lda) delle news in storia. Ogni rappresentazione contiene una
 # lista di tuple (etichetta, peso). Per tfidf le etichette sono le parole, per lda il numero del topic
 # ID : lista degli ID delle news le cui rappresentazioni sono in lista_tuple
 
-def profilo(storia, lista_tuple, ID):
+def profilo(storia, diz_grande):
    testi=[]#lista delle rappresentazioni concatenate di tutte le news lette dall'utente
-   for j in range(len(storia)): # selezioniamo solo le news in storia
-       if storia[j] in ID:
-           testi.extend(lista_tuple[j])
+   for articolo in storia:  # selezioniamo solo le news in storia
+       testi.extend(diz_grande[articolo])
    t = list(zip(*testi))  # separiamo le tuple(etichetta, peso)
    etichetta=list(t[0])
    peso=list(t[1])
@@ -29,6 +46,8 @@ def profilo(storia, lista_tuple, ID):
    if len(profilo)>1000:
         profilo=dict(sorted(profilo.items(), key=lambda item: item[1])[0:1000])
    return profilo
+
+
 
 import tqdm
 import multiprocessing as mp
