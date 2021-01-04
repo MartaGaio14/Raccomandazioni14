c53dde82a68537c28324212958f643ee52c9b23d
MartaGaio14 <gaio.marta@gmail.com>
2021-01-04 20:58:01 +0100

    raccomandati sistemata

--- raccomandazioni.py
+++ raccomandazioni.py
@@ -1,16 +1,17 @@
 import multiprocessing as mp
 import threading
-import tqdm
 
 #pos_utente è la posizione dell'utente in ID_utente
 #tipo è "lda" o "tfidf"
 def raccomandati(pos_utente, tipo, N, ID_test, risultati):
-    inizio = (pos_utente + 1) * len(ID_test) + 1
-    fine = (pos_utente + 2) * len(ID_test)
+    inizio=pos_utente*len(ID_test)
+    fine=inizio+len(ID_test)
     ut = risultati[inizio:fine]
-    top_tipo = ut.sort_values(by=[tipo], ascending=False)[0:N]
-    top_tipo = top_tipo.reset_index(drop=True)
-    return top_tipo
+    ut = ut.sort_values(by=[tipo], ascending=False)
+    ut = ut.reset_index(drop=True)
+    top_tipo=ut[0:N]
+    return list(top_tipo.NID)
+
 
 #racc lista delle news raccomandate per l'utente (output raccomandati (lda o tfidf))
 #storia lista delle news lette dall'utente (n_test[i])
@@ -22,16 +23,19 @@ def confusion_matrix(pos_utente,storia,tipo, N, ID_test, risultati, coda=None):
     fp = N - tp  # false positive
     fn = len(storia) - tp  # false negative
     tn = len(ID_test) - len(storia) - fp  # true negative
+    precision=tp/(tp+fp)
+    recall=tp/(tp+fn)
+    return(tp, fn, fp, tn)
     if coda is None:#se è non valorizzato ritorno il risultato
-        return (tp, fn, fp, tn)
+        return (precision, recall)
     else:#altrimenti accodo il risultato che ho trovato
-        coda.put((tp, fn, fp, tn))
+        coda.put((precision, recall))
 
-def confusion_matrix_par(n_test, tipo, N, ID_test, risultati):
+def confusion_matrix_par(n_test, tipo, N,  ID_test, risultati):
     coda = mp.Queue()
     threads = [threading.Thread(target=confusion_matrix, args=(pos_utente,n_test[pos_utente]),
         kwargs={"tipo":tipo, "N":N, "ID_test": ID_test, "risultati": risultati, "coda": coda})
-               for pos_utente in tqdm.tqdm(range(len(n_test)))]
+               for pos_utente in range(len(n_test))]
     for t in threads:
         t.start()
     x=[coda.get() for t in threads]
