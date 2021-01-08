724e957294a2229bde566700172a3a3313c01749
irene_chioatto <ire.c98@gmail.com>
2021-01-08 20:03:36 +0100

    nuove funzioni

--- raccomandazioni.py
+++ raccomandazioni.py
@@ -1,6 +1,6 @@
 import multiprocessing as mp
 import threading
-
+from sklearn.metrics import ndcg_score
 
 # pos_utente è la posizione dell'utente in ID_utente (da 1 a 1000)
 # tipo è il tipo di rappresentazione, "lda" o "tfidf"
@@ -11,6 +11,18 @@ def raccomandati(pos_utente, tipo, N, ID_test, risultati):
     fine = inizio + len(ID_test)
     ut = risultati[inizio:fine] #selezione dati relativi all'utente in questione
     ut = ut.sort_values(by=[tipo], ascending=False) #ordine decrescente dei dati rispetto alla similarità
+    ut = ut.reset_index(drop=True)
+    top_tipo = ut[0:N]
+    return list(top_tipo.NID)
+
+def raccomandati2(pos_utente, tipo, N, imp, risultati):
+    inizio = 0
+    if pos_utente != 0:
+        for i in range(pos_utente - 1):
+            inizio += len(imp[i])
+    fine = inizio + len(imp[pos_utente])
+    ut = risultati[inizio:fine]  # selezione dati relativi all'utente in questione
+    ut = ut.sort_values(by=[tipo], ascending=False)  # ordine decrescente dei dati rispetto alla similarità
     ut = ut.reset_index(drop=True)
     top_tipo = ut[0:N]
     return list(top_tipo.NID)
@@ -46,3 +58,21 @@ def confusion_matrix_par(n_test, tipo, N, ID_test, risultati):
     for t in threads:
         t.join()  # blocca il MainThread finché t non è completato
     return x
+
+#per ogni utente vengono inseriti imp: il dizionario contenente la sua impression, racc: la lista delle news raccomandate
+def input_ndcg(imp, racc):
+    true = list(imp.values())
+    ID = list(imp.keys())
+    prev = []
+    for articolo in ID:
+        if articolo in racc:
+            prev.append(1)
+        else:
+            prev.append(0)
+    return true, prev
+
+#calcola il punteggio ndcg
+def ndcg(pos_utente, tipo, N, imp, risultati):
+    racc = raccomandati2(pos_utente, tipo,  N, imp, risultati)
+    true, prev = input_ndcg(imp[pos_utente], racc)
+    return ndcg_score(true, prev)
